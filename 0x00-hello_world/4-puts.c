5d826d80e6dcc61e7d537ca92b6efcf38ad82876
soon <minamoreed68@gmail.com>
2023-03-20 00:21:56 +0200

    a

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Descrition:A C program that prints with put function
+ *
+ * Return: Alawys 0 (succes)
+*/
+
+int main (void) 
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
